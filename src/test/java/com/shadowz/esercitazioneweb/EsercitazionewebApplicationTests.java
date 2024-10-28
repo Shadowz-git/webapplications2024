d869d4fd1ba99ca9b7eacfb6d1c673793f8157dc
Shadowz <sh4dowswat@gmail.com>
2024-10-28 18:28:02 +0100

    Prova commit

---
+++ src/test/java/com/shadowz/esercitazioneweb/EsercitazionewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowz.esercitazioneweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercitazionewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
